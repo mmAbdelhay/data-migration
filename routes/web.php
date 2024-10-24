59318e4fb07e3d8b6452ed346a5f567bad051233
Muhammad AbdElHay <muhammad.abdelhay@vodafone.com>
2024-10-25 01:38:25 +0300

    added some html

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use MuhammadAbdElHay\DataMigration\Http\Controllers\DataMigrationController;
 
 Route::get('data-migration', [DataMigrationController::class, 'index'])->name('data-migration.index');
+Route::get('api/data-structure', [DataMigrationController::class, 'getDatabaseStructure'])->name('data-migration.data-structure');
--- src/Http/Controllers/DataMigrationController.php
+++ src/Http/Controllers/DataMigrationController.php
@@ -4,11 +4,30 @@ namespace MuhammadAbdElHay\DataMigration\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 class DataMigrationController extends Controller
 {
     public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
     {
         return view('data-migration::data-migration');
+    }
+
+    public function getDatabaseStructure(): \Illuminate\Http\JsonResponse
+    {
+        $databaseName = DB::getDatabaseName();
+        $results = DB::select("
+            SELECT TABLE_NAME as table_name, COLUMN_NAME as column_name
+            FROM information_schema.columns
+            WHERE table_schema = ?
+            ORDER BY TABLE_NAME, ORDINAL_POSITION
+        ", [$databaseName]);
+
+        $structure = [];
+        foreach ($results as $row) {
+            $structure[$row->table_name][] = $row->column_name;
+        }
+
+        return response()->json($structure);
     }
 }
